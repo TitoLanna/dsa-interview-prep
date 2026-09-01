9407a7f5a16cd5204c32b6a707eabb0c6dc003b2
Tito Lanna <matthiasotoo1z@gmail.com>
2026-09-01 12:12:43 -0500

    solved the subarray sum  equal k leetcode question

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ Data Structures and Algorithms interview preparation using Python.
 
 ### Statistics
 
-- Problems completed: **9**
+- Problems completed: **10**
 - Easy: **3**
-- Medium: **5**
+- Medium: **6**
 - Hard: **0**
 - Total reviews: **4**
 - Mastered: **0**
@@ -34,6 +34,7 @@ Data Structures and Algorithms interview preparation using Python.
 | 5 | Product of Array Except Self | Medium | Prefix Sum/Array | 2026-08-27 | 0 | - |  Learning |
 | 6 | Maximum Subarray | Medium | Array/Running State/Kadane/ | 2026-08-27 | 0 | - |  Learning |
 | 7 | Spiral Matrix | Medium | Array /Matrix/Boundary Simulation | 2026-08-28 | 0 | - |  Learning |
+| 8 | Subarray Sum Equals K | Medium | Array, Hash Table Prefix Sum | 2026-09-01 | 0 | - |  Learning |
 |  |  |  |  | 2026-08-27 | 0 | - |  Learning |
 | Remove Duplicates from Sorted Array | Remove Duplicates from Sorted Array | Medium | Two Pointers | - | 1 | 2026-08-31 |  Reviewing |
 
--- arrays/Subarray Sum Equals K.py
+++ arrays/Subarray Sum Equals K.py
@@ -17,7 +17,8 @@ Input: nums = [1,2,3], k = 3
 Output: 2
 """
 
-def subbarraySum(nums,k):
+def subarraySum(nums,k):
+    """This is the brute force approach """
     freq ={}
     ans=0
     n=len(nums)
@@ -32,6 +33,51 @@ def subbarraySum(nums,k):
                ans+=1
       
             
-    return ans, freq
-nums = [1,2,3]
-print(subbarraySum(nums,3))
+    return ans
+#The Prefix Sum and Hashmap approach
+
+def sub_array_Sum (nums, k):
+    
+    res =0
+    cur_sum=0
+    freq = {0:1}
+    for num in nums:
+        cur_sum+= num
+        diff = cur_sum -k
+        
+        res += freq.get(diff,0)
+        freq[cur_sum]=1+freq.get(cur_sum,0)
+    return res
+
+#Test Cases
+
+test_cases =[
+    {
+        "input":[1,1,1],
+        "k":2,
+        "exp_output":2
+    },
+    { 
+        "input":[1,2,3],
+        "k":3,
+        "exp_output":2
+    }
+]
+for i,test in enumerate(test_cases):
+    nums,k,expected= test["input"],test["k"],test["exp_output"]
+    result = sub_array_Sum(nums,k)
+    isPassed = True 
+    if expected!= result:
+        isPassed =False
+        
+    if isPassed:
+        print(f"\n Loading...\ntest_[{i+1}]")
+        print(f"{nums}:")
+        print(f"result: {result}")
+        print("Passed !")
+    else:
+        print(f"\nLoading... \ntest_[{i+1}]")
+        print(f"{nums}:")
+        print(f"result: {result}\t",f"Expected result:{expected}")
+        print("\n Failed ! ")
+         
