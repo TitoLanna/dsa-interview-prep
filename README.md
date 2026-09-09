80d2874f07a01d796382ce7c9eba8f9a556f5f42
Tito Lanna <matthiasotoo1z@gmail.com>
2026-09-09 00:06:20 -0500

    Solved  Longest Repeating Character Replacement problem

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Data Structures and Algorithms interview preparation using Python.
 
 ### Statistics
 
-- Problems completed: **22**
+- Problems completed: **23**
 - Easy: **8**
 - Medium: **13**
 - Hard: **1**
@@ -49,6 +49,7 @@ Data Structures and Algorithms interview preparation using Python.
 | 20 | Maximum Average Subarray I | Easy | Sliding Window | 2026-09-05 | 0 | - |  Learning |
 | 21 | Longest Substring Without Repeating Characters | Medium | Hash Table/ Sliding Window | 2026-09-06 | 0 | - |  Learning |
 | 22 | Minimum Size Subarray Sum | Medium | Sliding Window | 2026-09-07 | 0 | - |  Learning |
+| 23 | Longest Repeating Character Replacement | Meduim | Window Invariant | 2026-09-09 | 0 | - |  Learning |
 
 <!-- PROGRESS_END -->
 
---
+++ sliding_window/Longest Repeating Character Replacement.py
@@ -0,0 +1,40 @@
+
+""" 
+    Given a string s and an integer k.
+    You can choose any character of the string and change it 
+    to  any other uppercase English character. 
+    You can perform this operation at most k times.
+"""
+def characterReplacement(s,k):
+    """ 
+    
+   
+    
+    Args: 
+        s (str): The string .Example  s="ABAB"
+        k (int): The integer . Example k =2
+        
+    Returns:
+        int : The length of the longest substring containing the same letter
+
+    """
+    left = 0
+    longest =0
+    count =[0]* 26
+    
+    for r in range(len(s)):
+        count[ord(s[r]) -65]+=1
+        
+        while (r-left+1) - max(count)> k:
+            count[ord(s[left])-65]-=1
+            left+=1
+        longest= max(longest,r-left+1)
+        
+    return longest
+   
+        
+
+
+s=  "ABAB"
+k = 2
+print(characterReplacement(s,k))
